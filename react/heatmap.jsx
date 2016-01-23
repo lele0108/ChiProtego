a92d49448a039152cf7f2a0681f83774d89007db
James Anderson <me@jamesaanderson.com>
2016-01-23 09:03:45 -0500

    Pass map options

--- react/heatmap.jsx
+++ react/heatmap.jsx
@@ -1,13 +1,8 @@
-var CRIMES_JSON = 'https://api.everyblock.com/content/philly/topnews/?format=json&schema=crime&schema=announcements&token=90fe24d329973b71272faf3f5d17a8602bff996b';
+var CRIMES_API = 'https://api.everyblock.com/content/philly/topnews/?format=json&schema=crime&schema=announcements&token=90fe24d329973b71272faf3f5d17a8602bff996b';
 
 var HeatMap = React.createClass({
   componentDidMount: function() {
-    var map = new google.maps.Map(ReactDOM.findDOMNode(this), {
-      zoom: 13,
-      center: {lat: 39.9521950, lng: -75.1911030},
-      mapTypeId: google.maps.MapTypeId.SATELLITE
-    });
-
+    var map = new google.maps.Map(ReactDOM.findDOMNode(this), this.props.options);
     var heatmap = new google.maps.visualization.HeatmapLayer({
       data: this.props.points,
       map: map
@@ -24,7 +19,8 @@ var HeatMap = React.createClass({
 var CrimesMap = React.createClass({
   getInitialState: function() {
     return {
-      points: null
+      points: null,
+      options: {zoom: 13, center: {lat: 39.9521950, lng: -75.1911030}, mapTypeId: google.maps.MapTypeId.SATELLITE}
     };
   },
 
@@ -32,7 +28,7 @@ var CrimesMap = React.createClass({
     var that = this;
 
     $.ajax({
-      url: CRIMES_JSON,
+      url: CRIMES_API,
       dataType: 'json',
       type: 'GET',
       success: function(data) {
@@ -49,7 +45,7 @@ var CrimesMap = React.createClass({
 
   render: function() {
     if (this.state.points) {
-      return <HeatMap points={this.state.points} />
+      return <HeatMap points={this.state.points} options={this.state.options} />
     } else {
       return <div>Loading...</div> 
     }
