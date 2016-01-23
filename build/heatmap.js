20c6dfd1aef59024028a2ef14327a431e04a95d0
James Anderson <me@jamesaanderson.com>
2016-01-23 03:41:47 -0500

    Crimes data

--- build/heatmap.js
+++ build/heatmap.js
@@ -1,13 +1,35 @@
-var HeatMap = React.createClass({
-  displayName: 'HeatMap',
+var CRIMES_JSON = 'https://api.everyblock.com/content/philly/topnews/?format=json&schema=crime&schema=announcements&token=90fe24d329973b71272faf3f5d17a8602bff996b';
+
+var Crimes = React.createClass({
+  displayName: 'Crimes',
+
+  getInitialState: function () {
+    return {
+      crimes: []
+    };
+  },
+
+  componentDidMount: function () {
+    var that = this;
+
+    $.ajax({
+      url: CRIMES_JSON,
+      dataType: 'json',
+      type: 'GET',
+      success: function (data) {
+        that.setState({ crimes: data.results });
+      }
+    });
+  },
 
   render: function () {
+    console.log(this.state.crimes);
     return React.createElement(
       'div',
       null,
-      'Heat Map'
+      'Hello World'
     );
   }
 });
 
-ReactDOM.render(React.createElement(HeatMap, null), document.getElementById('react'));
+ReactDOM.render(React.createElement(Crimes, null), document.getElementById('react'));
--- react/heatmap.jsx
+++ react/heatmap.jsx
@@ -1,7 +1,29 @@
-var HeatMap = React.createClass({
+var CRIMES_JSON = 'https://api.everyblock.com/content/philly/topnews/?format=json&schema=crime&schema=announcements&token=90fe24d329973b71272faf3f5d17a8602bff996b';
+
+var Crimes = React.createClass({
+  getInitialState: function() {
+    return {
+      crimes: []
+    };
+  },
+
+  componentDidMount: function() {
+    var that = this;
+
+    $.ajax({
+      url: CRIMES_JSON,
+      dataType: 'json',
+      type: 'GET',
+      success: function(data) {
+        that.setState({crimes: data.results});
+      }
+    });
+  },
+
   render: function() {
-    return <div>Heat Map</div>;
+    console.log(this.state.crimes);
+    return <div>Hello World</div>  
   }
 });
 
-ReactDOM.render(<HeatMap />, document.getElementById('react'));
+ReactDOM.render(<Crimes />, document.getElementById('react'));
